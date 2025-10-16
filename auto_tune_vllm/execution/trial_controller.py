8c2259841ab1d5f711b5a3704ee32f08a92ee8f0
Thameem Abbas <thibrahi@redhat.com>
2025-10-16 16:09:25 -0400

    Update auto_tune_vllm/execution/trial_controller.py
    Co-authored-by: Andy Huynh <ahuynh@redhat.com>

--- auto_tune_vllm/execution/trial_controller.py
+++ auto_tune_vllm/execution/trial_controller.py
@@ -347,7 +347,7 @@ class BaseTrialController(TrialController):
                             timeout=0.2
                         )
                         return is_cancelled
-                    except Exception:
+                    except (Exception, GetTimeoutError) as _:
                         return False
                 return self._cancellation_requested
             
